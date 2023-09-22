b174f433eac13ef29a16790dca3fc0289fad7430
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-22 16:28:02 +0300

    assertIterableEquals

--- src/test/java/AssertTest.java
+++ src/test/java/AssertTest.java
@@ -1,6 +1,11 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
 public class AssertTest {
     @Test
     public void test() {
@@ -18,9 +23,14 @@ public class AssertTest {
 //        Assertions.assertEquals(s1, s2, "Strings the same");
 //        Assertions.assertNotEquals(s1, s2, "Strings the same");
 
-        int [] array1 = new int[]{1,2};
-        int [] array2 = new int[]{0,2};
-        int [] array3 = new int[]{1,2,3};
-        Assertions.assertArrayEquals(array1, array3, "Arrays not equals");
+//        int [] array1 = new int[]{1,2};
+//        int [] array2 = new int[]{0,2};
+//        int [] array3 = new int[]{1,2,3};
+//        Assertions.assertArrayEquals(array1, array3, "Arrays not equals");
+        Iterable<Integer> list1 = new ArrayList<>(Arrays.asList(1,2,3,4,5,-1,-2));
+        Iterable<Integer> list2 = new ArrayList<>(Arrays.asList(1,2,3,4,5,-1,-2));
+
+        Assertions.assertIterableEquals(list1, list2);
+
     }
 }
