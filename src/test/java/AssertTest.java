e306550dd7bed6966960956fd2c744c5e87d329d
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-22 16:30:05 +0300

    assertLinesMatch

--- src/test/java/AssertTest.java
+++ src/test/java/AssertTest.java
@@ -3,7 +3,6 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 
 public class AssertTest {
@@ -27,10 +26,13 @@ public class AssertTest {
 //        int [] array2 = new int[]{0,2};
 //        int [] array3 = new int[]{1,2,3};
 //        Assertions.assertArrayEquals(array1, array3, "Arrays not equals");
-        Iterable<Integer> list1 = new ArrayList<>(Arrays.asList(1,2,3,4,5,-1,-2));
-        Iterable<Integer> list2 = new ArrayList<>(Arrays.asList(1,3,4,5,-1,-2));
+//        Iterable<Integer> list1 = new ArrayList<>(Arrays.asList(1,2,3,4,5,-1,-2));
+//        Iterable<Integer> list2 = new ArrayList<>(Arrays.asList(1,3,4,5,-1,-2));
+//
+//        Assertions.assertIterableEquals(list1, list2);
 
-        Assertions.assertIterableEquals(list1, list2);
-
+        List<String> list1 = new ArrayList<>(Arrays.asList("Some string", "Another string"));
+        List<String> list2 = new ArrayList<>(Arrays.asList("Some", "string", "Another string"));
+        Assertions.assertLinesMatch(list1, list2);
     }
 }
