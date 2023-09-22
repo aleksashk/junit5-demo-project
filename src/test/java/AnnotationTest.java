fec7d967df910b6101e1b787a412cda869698518
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-22 13:36:07 +0300

    @BeforeEach, @AfterEach

--- src/test/java/AnnotationTest.java
+++ src/test/java/AnnotationTest.java
@@ -1,8 +1,28 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class AnnotationTest {
+
+    @BeforeEach
+    public void beforeEach(){
+        System.out.println("@BeforeEach");
+    }
+
+
     @Test
     public void test(){
         System.out.println("@Test");
     }
+
+    @Test
+    public void test2(){
+        System.out.println("@Test2");
+    }
+
+    @AfterEach
+    public void afterEach(){
+        System.out.println("@AfterEach");
+    }
+
 }
